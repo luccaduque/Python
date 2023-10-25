eb5fd27d7f94b0f16d324b046277d6a796df4248
Lucca S. Duque <subsgg932@gmail.com>
2023-10-24 22:29:32 -0300

    commit

--- estudando prova/anotacoes rapidas.py
+++ estudando prova/anotacoes rapidas.py
@@ -1,7 +1,7 @@
 #maiorIMPAR = 0
 #menorIMPAR = 0
-#if (n > maiorPAR):
+#if (n > maiorPAR): # n MAIOR QUE
 #   maiorPAR = n
 
-#if (menorIMPAR == 0 or n < menorIMPAR):
+#if (menorIMPAR == 0 or n < menorIMPAR):  # n MENOR QUE 
 #   menorIMPAR = n
